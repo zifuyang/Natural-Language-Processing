500139fb50949ca5af305f2296c49b4ed63f35bc
gravo-lan <alvintsolh@gmail.com>
2023-04-19 15:18:33 +0800

    add verbose getopt arg

--- nlp.py
+++ nlp.py
@@ -3,10 +3,6 @@ from nltk.corpus import wordnet, PlaintextCorpusReader
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
-nltk.download('wordnet', quiet=True)
-nltk.download('punkt', quiet=True)
-nltk.download('averaged_perceptron_tagger', quiet=True)
-
 def load_text(file_path: str) -> str:
     if not os.path.exists(file_path):
         raise FileNotFoundError(f"File {file_path} not found")
@@ -67,7 +63,11 @@ def semantic_analysis(text):
     lemmatized_tokens = lemmatize_tokens(tokens)
     return ' '.join(lemmatized_tokens)
 
-def generate_text(seed_text, length: int, order: int) -> str:
+def generate_text(seed_text, length: int, order: int, verbose: bool) -> str:
+    nltk.download('wordnet', quiet=not(verbose))
+    nltk.download('punkt', quiet=not(verbose))
+    nltk.download('averaged_perceptron_tagger', quiet=not(verbose))
+
     generated_text = seed_text
     original_text = seed_text
     corrected_text = semantic_analysis(generated_text)
@@ -102,14 +102,17 @@ def main() -> None:
     file_path = None
     order = 1
     length=200
+    verbose=False
 
     try:
-        arguments, _ = getopt.getopt(argumentList, "hi:o:f:l:", ["ifile=", "order=", "file=", "length="])
+        arguments, _ = getopt.getopt(argumentList, "hvi:o:f:l:", ["ifile=", "order=", "file=", "length="])
         if len(arguments) > 0:
             for currentArgument, currentValue in arguments:
                 if currentArgument == "-h":
                     print("Usage: python program.py -i <inputfile> -o <order>")
                     sys.exit()
+                if currentArgument == "-v":
+                    verbose=True
                 elif currentArgument in ("-i", "--ifile", "-f", "--file"):
                     file_path = currentValue
                 if currentArgument in ("-o", "--order"):
@@ -142,7 +145,7 @@ def main() -> None:
         print(f"File not found: {e}")
         sys.exit(2)
     
-    generated_text = generate_text(seed_text, length, order)
+    generated_text = generate_text(seed_text, length, order, verbose)
     leading=0
     for i in generated_text:
         if not(i.lower() in string.ascii_lowercase):
@@ -166,9 +169,9 @@ def main() -> None:
                 line_length += len(word) + 1
 
 def test():
-    print(generate_text(load_text("corpora/treasureisland.txt"), 200, 1))
-    print(generate_text(load_text("corpora/treasureisland.txt"), 200, 2))
-    print(generate_text(load_text("corpora/treasureisland.txt"), 200, 3))
+    print(generate_text(load_text("corpora/treasureisland.txt"), 200, 1, True))
+    print(generate_text(load_text("corpora/treasureisland.txt"), 200, 2, True))
+    print(generate_text(load_text("corpora/treasureisland.txt"), 200, 3, True))
 
 if __name__ == '__main__':
     main()
